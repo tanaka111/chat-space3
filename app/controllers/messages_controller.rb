9a79d11793d506bed15cfcf0cd9a7128ca2bc5a8
tanaka111 <tanaka.naoki7014@gmail.com>
2019-01-22 13:10:36 +0900

    コメント削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -33,13 +33,3 @@ end
 
 
 
-# def create
-#     @message = @group.messages.new(message_params)
-#     if @message.save
-#       redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
-#     else
-#       @message = @group.messages.includes(:user)
-#       flash.now[alert] = "メッセージを入力してください"
-#       render :index
-#     end
-#   end
