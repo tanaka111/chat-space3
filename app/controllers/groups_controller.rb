76fcd3ddefed93835cc4ccd94b4623152af0c843
tanaka111 <tanaka.naoki7014@gmail.com>
2019-01-22 10:54:42 +0900

    インクルメンタルサーチ  コントローラー追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,11 +6,11 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.users << current_user
   end
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: "グループを作成しました"
     else
@@ -19,6 +19,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
